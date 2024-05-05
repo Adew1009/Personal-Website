73e19bc411d46616ed23222ee120ccf3d8c8510a
Andrew_Dew <adew1009@gmail.com>
2024-05-05 13:37:13 -0500

    added app import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
+import { app } from "./components/firebase";
 
 function App() {
   return (
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -21,4 +21,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app)
