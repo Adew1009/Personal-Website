902f1b841f4d2a01df846440bbf8e5e0ce0535de
Andrew_Dew <adew1009@gmail.com>
2024-05-06 18:11:05 -0500

    added LinkedIn and GitHub icon and link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./mode_toggle";
 const NavBar = () => {
   return (
     <nav className="items-center justify-between hidden h-max w-full md:flex md:w-auto md:order-1">
-      <ul className="flex flex-col font-medium md:p-0 mt-2 pt-2 md:space-x-2 rtl:space-x-reverse md:flex-row md:mt-0">
+      <ul className="flex flex-col font-xl   md:flex-row md:mt-0">
         <li>
           <Link
             to="/"
@@ -21,8 +21,38 @@ const NavBar = () => {
             &emsp; &emsp; About
           </Link>
         </li>
-      </ul>
-      <ModeToggle />
+        <li>
+          <Link
+            to="https://drive.google.com/file/d/1ZczosSwEbLCMnCl2B6bCUmDoJKXPYb_g/view?usp=drive_link"
+            // className="shadow-md rounded-full pt-2 pb-2 pr-4 pl-4 no-underline flex justify-center text-gray-900 hover:bg-orange-100"
+          >
+            &emsp; &emsp; Resume
+          </Link>
+        </li>
+      </ul>{" "}
+      <div className="">
+        <a
+          title="VS Code"
+          href="https://www.linkedin.com/in/andrew-dew/"
+          target="blank"
+        >
+          <img
+            height="30"
+            width="30"
+            src="https://skillicons.dev/icons?i=linkedin"
+            alt="LinkedIn icon"
+          />
+        </a>
+        <a title="VS Code" href="https://github.com/Adew1009" target="blank">
+          <img
+            height="30"
+            width="30"
+            src="https://skillicons.dev/icons?i=github"
+            alt="LinkedIn icon"
+          />
+        </a>
+        <ModeToggle />
+      </div>
     </nav>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,12 +5,21 @@ const HomePage = () => {
   return (
     <>
       <main>
-        <div className="home-content">
-          <h1 className="text-6xl">Hi, I'm Andrew Dew</h1> <br></br>
+        <div>
+          <h1 className="heading text-8xl home-content">Hi, I'm Andrew Dew</h1>
+          <br></br>
+        </div>
+        <section className="heading home-content">
           <article>I am Full-Stack Software Engineer</article>
-        </div>{" "}
-        f
-      </main>{" "}
+        </section>
+        <iframe
+          src="/Users/andrew-dew/projects/Personal-Website/src/components/Andrew Dew Resume.pdf"
+          width="100%"
+          height="600px"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </main>
     </>
   );
 };
